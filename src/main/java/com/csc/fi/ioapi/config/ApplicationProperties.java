022bf44ed944c2ff25a22543bc2f3f0b3d3be577
amiika <amiika@gmail.com>
2015-10-02 13:36:45 +0300

    Renamed Endpoint (Manager) to more generic ApplicationProperties

---
+++ src/main/java/com/csc/fi/ioapi/config/ApplicationProperties.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.csc.fi.ioapi.config;
+
+import com.csc.fi.ioapi.utils.PropertyUtil;
+
+/**
+ *
+ * @author malonen
+ */
+public class ApplicationProperties {
+    
+public static String getEndpoint() {
+    return PropertyUtil.getProperty("application.endpoint");
+}
+
+public static boolean getDebugMode() {
+    return Boolean.parseBoolean(PropertyUtil.getProperty("application.debug"));
+} 
+
+public static String getGroupDomain() {
+    return PropertyUtil.getProperty("application.groupdomain");
+} 
+    
+}
