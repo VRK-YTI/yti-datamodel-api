a0678cd37b1c022cfeeb318121f5a9039c10f2c3
amiika <amiika@gmail.com>
2015-12-11 09:05:53 +0200

    Added no cache

--- src/main/java/com/csc/fi/ioapi/config/CORSFilter.java
+++ src/main/java/com/csc/fi/ioapi/config/CORSFilter.java
@@ -1,10 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Licensed under the European Union Public Licence (EUPL) V.1.1 
  */
 package com.csc.fi.ioapi.config;
 
+import com.google.common.net.HttpHeaders;
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerResponse;
 import com.sun.jersey.spi.container.ContainerResponseFilter;
@@ -14,34 +13,19 @@ import com.sun.jersey.spi.container.ContainerResponseFilter;
  * @author malonen
  */
 public class CORSFilter implements ContainerResponseFilter {
- 
+
     @Override
     public ContainerResponse filter(ContainerRequest req, ContainerResponse resp) {
- 
-       resp.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
-       resp.getHttpHeaders().add("Access-Control-Allow-Credentials","true");
-       resp.getHttpHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-       resp.getHttpHeaders().add("Access-Control-Request-Headers","origin, content-type, accept, authorization");
-       resp.getHttpHeaders().add("Access-Control-Allow-Headers","Content-Type, Accept, X-Requested-With");
-       
-       return resp;
-       
-        /* Another way to do it? */
-        /*
-        ResponseBuilder resp = Response.fromResponse(contResp.getResponse());
-        resp.header("Access-Control-Allow-Origin", "*")
-            .header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
- 
-        String reqHead = req.getHeaderValue("Access-Control-Request-Headers");
- 
-        if(null != reqHead && !reqHead.equals("")){
-            resp.header("Access-Control-Allow-Headers", reqHead);
-        }
- 
-        contResp.setResponse(resp.build());
-        return contResp;
-        */
-       
-          }
- 
+
+        resp.getHttpHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        resp.getHttpHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+        resp.getHttpHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        resp.getHttpHeaders().add(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS, "origin, content-type, accept, authorization");
+        resp.getHttpHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "Content-Type, Accept, X-Requested-With");
+        resp.getHttpHeaders().add(HttpHeaders.CACHE_CONTROL, "no-cache");
+
+        return resp;
+
+    }
+
 }
