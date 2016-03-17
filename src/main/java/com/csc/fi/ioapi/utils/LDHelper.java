2f16e0d26c4219cd54ed6bc2ddd269287b68232a
amiika <amiika@gmail.com>
2016-03-17 16:35:11 +0200

    sun.text.normalizer.NormalizerImpl bug fixed for now

--- src/main/java/com/csc/fi/ioapi/utils/LDHelper.java
+++ src/main/java/com/csc/fi/ioapi/utils/LDHelper.java
@@ -123,40 +123,6 @@ public class LDHelper {
         return name;
     }
     
-    private static final String tab00c0 = 
-    "AAAAAAACEEEEIIII" +
-    "DNOOOOO\u00d7\u00d8UUUUYI\u00df" +
-    "aaaaaaaceeeeiiii" +
-    "\u00f0nooooo\u00f7\u00f8uuuuy\u00fey" +
-    "AaAaAaCcCcCcCcDd" +
-    "DdEeEeEeEeEeGgGg" +
-    "GgGgHhHhIiIiIiIi" +
-    "IiJjJjKkkLlLlLlL" +
-    "lLlNnNnNnnNnOoOo" +
-    "OoOoRrRrRrSsSsSs" +
-    "SsTtTtTtUuUuUuUu" +
-    "UuUuWwYyYZzZzZzF";
-
-    /* From http://stackoverflow.com/a/10831704/4054733 */
-    public static String removeDiacritic(String source) {
-        char[] vysl = new char[source.length()];
-        char one;
-        for (int i = 0; i < source.length(); i++) {
-            one = source.charAt(i);
-            if (one >= '\u00c0' && one <= '\u017f') {
-                one = tab00c0.charAt((int) one - '\u00c0');
-            }
-            vysl[i] = one;
-        }
-        return new String(vysl);
-    }
-    
-    /* TODO: FIX dependency java.lang.NoClassDefFoundError: Could not initialize class sun.text.normalizer.NormalizerImpl
-       and use Normalizer instead? */
-   /* public static String removeAccents(String text) {
-        return text == null ? null : removeDiacritic(text);
-    }*/
-    
    public static String removeAccents(String text) {
     return text == null ? null :
         Normalizer.normalize(text, Form.NFD)
