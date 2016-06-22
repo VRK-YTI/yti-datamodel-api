4672675d3e3b3eff1aff3ccb9c3f0b9f516ba52c
amiika <amiika@gmail.com>
2016-06-22 19:36:20 +0300

    Lang parameter not required in new /conceptScheme API

--- src/main/java/com/csc/fi/ioapi/api/concepts/ConceptsSchemes.java
+++ src/main/java/com/csc/fi/ioapi/api/concepts/ConceptsSchemes.java
@@ -5,27 +5,16 @@ package com.csc.fi.ioapi.api.concepts;
 
 import com.csc.fi.ioapi.config.EndpointServices;
 import com.csc.fi.ioapi.utils.JerseyFusekiClient;
-import com.csc.fi.ioapi.utils.LDHelper;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.WebResource.Builder;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
 import com.wordnik.swagger.annotations.ApiResponse;
 import com.wordnik.swagger.annotations.ApiResponses;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
-import javax.ws.rs.core.Response.Status;
 
 /**
  * REST Web Service
@@ -48,13 +37,8 @@ public class ConceptsSchemes {
       @ApiResponse(code = 406, message = "Term not defined"),
       @ApiResponse(code = 500, message = "Internal server error")
   })
-  public Response vocab(
-          @ApiParam(value = "Lang") 
-          @QueryParam("lang") String lang) {
-   
+  public Response vocab() {
            return JerseyFusekiClient.getGraphResponseFromService("urn:csc:schemes", services.getTempConceptReadWriteAddress());
-    
-      
   }
   
   
