01bd3b72de9e9ae691097fe7c619427ae3acaba6
amiika <amiika@gmail.com>
2016-06-28 15:10:54 +0300

    Initial periodic update script

--- src/main/java/com/csc/fi/ioapi/config/StartUpListener.java
+++ src/main/java/com/csc/fi/ioapi/config/StartUpListener.java
@@ -10,6 +10,13 @@ import com.csc.fi.ioapi.utils.GraphManager;
 import com.csc.fi.ioapi.utils.GroupManager;
 import com.csc.fi.ioapi.utils.NamespaceManager;
 import com.csc.fi.ioapi.utils.OPHCodeServer;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -36,6 +43,8 @@ public class StartUpListener implements ServletContextListener {
         initCodeServers();
         loadSchemesFromFinto();
         
+     //   runPeriodicUpdates();
+        
     }
     
     @Override
@@ -50,6 +59,27 @@ public class StartUpListener implements ServletContextListener {
     
     if(!codeServer.status) logger.warning("Code server was not initialized!");
     
+    }
+    
+    private static void runPeriodicUpdates() {
+        /*
+        LocalDateTime localNow = LocalDateTime.now();
+        ZoneId currentZone = ZoneId.of("America/Los_Angeles");
+        ZonedDateTime zonedNow = ZonedDateTime.of(localNow, currentZone);
+        ZonedDateTime zonedNext5 ;
+        zonedNext5 = zonedNow.withHour(5).withMinute(0).withSecond(0);
+        if(zonedNow.compareTo(zonedNext5) > 0)
+            zonedNext5 = zonedNext5.plusDays(1);
+
+        Duration duration = Duration.between(zonedNow, zonedNext5);
+        long initialDelay = duration.getSeconds();
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1); */
+        /* TODO: Create runnable to loadSchemesFromFinto() */
+       /* scheduler.
+                .scheduleAtFixedRate(FintoLoad), initalDelay,
+                                      24*60*60, TimeUnit.SECONDS); */
+        
     }
     
     private static void loadSchemesFromFinto() {
