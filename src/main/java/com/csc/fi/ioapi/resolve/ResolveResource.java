e4d416d29aa96e7731028c97e435de3dcf4b9daa
amiika <amiika@gmail.com>
2016-08-23 14:45:58 +0300

    Cleaned code

--- src/main/java/com/csc/fi/ioapi/resolve/ResolveResource.java
+++ src/main/java/com/csc/fi/ioapi/resolve/ResolveResource.java
@@ -51,28 +51,11 @@ public class ResolveResource extends HttpServlet {
         String modelID = requestURI.substring(requestURI.lastIndexOf("/") + 1, requestURI.length());
         String graphName = GraphManager.getServiceGraphNameWithPrefix(modelID);
         
-        logger.info(graphName);
-        
         if(graphName==null) {
             logger.info("Graphname is null");
             response.sendError(HttpServletResponse.SC_NOT_FOUND);
-        }
+        } else {
         
-        /*
-        String modelURL = ApplicationProperties.getDefaultDomain()+"ns/"+modelID;
-       
-        
-        IRIFactory iriFactory = IRIFactory.iriImplementation();
-            IRI modelIRI;
-            try {
-                modelIRI = iriFactory.construct(modelURL);
-            }
-            catch (IRIException e) {
-                logger.warning("Invalid URL: "+modelURL); 
-               response.sendError(HttpServletResponse.SC_NOT_FOUND);
-            } 
-        */
-
                 if(rdfLang==null) {
                     logger.info("Redirecting to root");
                     response.sendRedirect("/#/model?urn="+graphName);
@@ -82,31 +65,7 @@ public class ResolveResource extends HttpServlet {
                     RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(dis);
                     dispatcher.forward(request,response);
                 }    
-            
-        
-           /*
-            response.setContentType("text/html;charset=UTF-8");    
-                try (PrintWriter out = response.getWriter()) {
-                    out.println("<!DOCTYPE html>");
-                    out.println("<html>");
-                    out.println("<head>");
-                    out.println("<title>Servlet ResolveResource</title>");            
-                    out.println("</head>");
-                    out.println("<body>");
-
-                    out.println("<h1>"+modelID+"</h1>");
-                    out.println("<h1>"+request.getRequestURI() + "</h1>");
-                    out.println("<h1>"+request.getQueryString() + "</h1>");
-                    out.println("<h1>"+request.getHeader("Accept") + "</h1>");
-                    if(rdfLang==null) out.println("<h1>NOT RDF</h1>");
-                    else out.println("<h1>"+rdfLang.getLabel()+"</h1>");
-                    out.println("</body>");
-                    out.println("</html>");
-                }
-
-            */
-
-
+        } 
       
     }
 
