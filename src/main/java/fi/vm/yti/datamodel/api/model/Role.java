213f75d872fa62e8ddb28fee2f1f9328066f7b79
Janne Mäki <janne.maki@evident.fi>
2017-11-09 10:39:54 +0200

    Remove no longer existing roles from the enum

--- src/main/java/fi/vm/yti/datamodel/api/model/Role.java
+++ src/main/java/fi/vm/yti/datamodel/api/model/Role.java
@@ -3,13 +3,9 @@ package fi.vm.yti.datamodel.api.model;
 public enum Role {
 
     ADMIN,
-    DATA_MODEL_ADMIN,
     DATA_MODEL_EDITOR,
-    TERMINOLOGY_ADMIN,
     TERMINOLOGY_EDITOR,
-    CODE_LIST_ADMIN,
-    CODE_LIST_EDITOR,
-    TRANSLATOR;
+    CODE_LIST_EDITOR;
 
     public static boolean contains(String roleString) {
         for (Role role : values()) {
