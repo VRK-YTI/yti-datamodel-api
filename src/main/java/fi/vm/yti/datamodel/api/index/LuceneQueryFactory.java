defbdc73bb5e8b02fcf5af9e736a3e8616f70730
Miika <amiika@gmail.com>
2019-06-03 15:23:16 +0300

    Added lucene query validator

--- src/main/java/fi/vm/yti/datamodel/api/index/LuceneQueryFactory.java
+++ src/main/java/fi/vm/yti/datamodel/api/index/LuceneQueryFactory.java
@@ -2,26 +2,49 @@ package fi.vm.yti.datamodel.api.index;
 
 import javax.inject.Singleton;
 
+import org.apache.lucene.queryparser.flexible.core.QueryNodeException;
+import org.apache.lucene.queryparser.flexible.standard.StandardQueryParser;
+import org.apache.lucene.search.Query;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.QueryStringQueryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Singleton
 @Service
 public class LuceneQueryFactory {
 
+    private static final Logger logger = LoggerFactory.getLogger(LuceneQueryFactory.class.getName());
     public QueryStringQueryBuilder buildPrefixSuffixQuery(String query) {
-        if (query.contains("*")) {
-            return QueryBuilders.queryStringQuery(query);
-        } else if (query.contains(" ")) {
-            String[] splittedQry = query.split("\\s+");
-            for (int i = 0; i < splittedQry.length; i++) {
-                splittedQry[i] = ("(" + splittedQry[i] + " OR *" + splittedQry[i] + " OR " + splittedQry[i] + "*)");
+        String parsedQuery = null;
+
+        if (!query.contains("*")) {
+            if (query.contains(" ")) {
+                String[] splittedQry = query.split("\\s+");
+                for (int i = 0; i < splittedQry.length; i++) {
+                    splittedQry[i] = ("(" + splittedQry[i] + " OR *" + splittedQry[i] + " OR " + splittedQry[i] + "*)");
+                }
+                parsedQuery = String.join(" AND ", splittedQry);
+            } else {
+                parsedQuery = query + " OR " + query + "* OR *" + query;
             }
-            return QueryBuilders.queryStringQuery(String.join(" AND ", splittedQry));
         } else {
-            return QueryBuilders.queryStringQuery(query + " OR " + query + "* OR *" + query);
+            parsedQuery = query;
         }
+
+        StandardQueryParser parser = new StandardQueryParser();
+        Query luceneQuery = null;
+
+        try {
+            parser.setAllowLeadingWildcard(true);
+            luceneQuery = parser.parse(parsedQuery, "");
+        } catch (QueryNodeException e) {
+            logger.warn("Failed to parse: "+parsedQuery);
+            throw new IllegalArgumentException("Invalid query");
+        }
+
+        return QueryBuilders.queryStringQuery(luceneQuery.toString());
     }
 
 }
