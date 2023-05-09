39f11c21ae929147440e6040500da777f82d7a9c
jkesanie <joonas.kesaniemi@csc.fi>
2023-05-09 11:05:10 +0300

    Removed endpoint related exception from service interface.

--- src/main/java/fi/vm/yti/datamodel/api/v2/service/StorageService.java
+++ src/main/java/fi/vm/yti/datamodel/api/v2/service/StorageService.java
@@ -2,11 +2,10 @@ package fi.vm.yti.datamodel.api.v2.service;
 
 import java.util.List;
 
-import fi.vm.yti.datamodel.api.v2.endpoint.error.StorageException;
 
 public interface StorageService {
 
-	public int storeSchemaFile(String schemaPID, String contentType, byte[] data) throws StorageException;
+	public int storeSchemaFile(String schemaPID, String contentType, byte[] data);
 	public StoredFile retrieveSchemaFile(String schemaPID, long fileID);
 	public List<StoredFile> retrieveAllSchemaFiles(String schemaPID);
 	
