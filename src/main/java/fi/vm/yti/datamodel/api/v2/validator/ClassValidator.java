182aeae2b32c3ffd4182a184525ee9444a6d5909
Kalle Kyttälä <kalle.kyttala@rebase.fi>
2023-07-21 09:19:55 +0300

    fix subclass and equivalent class validation

--- src/main/java/fi/vm/yti/datamodel/api/v2/validator/ClassValidator.java
+++ src/main/java/fi/vm/yti/datamodel/api/v2/validator/ClassValidator.java
@@ -43,7 +43,7 @@ public class ClassValidator extends BaseValidator implements
                 var asUri = NodeFactory.createURI(eqClass);
                 //if namespace is resolvable make sure class can be found in resolved namespace
                 if(jenaService.doesResolvedNamespaceExist(asUri.getNameSpace())
-                        && jenaService.doesResourceExistInImportedNamespace(asUri.getNameSpace(), asUri.getURI())){
+                        && !jenaService.doesResourceExistInImportedNamespace(asUri.getNameSpace(), asUri.getURI())){
                     addConstraintViolation(context, "class-not-found-in-resolved-namespace", "eqClass");
                 }
             });
@@ -57,7 +57,7 @@ public class ClassValidator extends BaseValidator implements
                 var asUri = NodeFactory.createURI(subClass);
                 //if namespace is resolvable make sure class can be found in resolved namespace
                 if(jenaService.doesResolvedNamespaceExist(asUri.getNameSpace())
-                        && jenaService.doesResourceExistInImportedNamespace(asUri.getNameSpace(), asUri.getURI())){
+                        && !jenaService.doesResourceExistInImportedNamespace(asUri.getNameSpace(), asUri.getURI())){
                     addConstraintViolation(context, "class-not-found-in-resolved-namespace", "subClassOf");
                 }
             });
